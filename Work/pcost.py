0d7f794089058c577ddcfc6fbf1a6373461391bd
Narek Baghdasaryan <narek2011n@gmail.com>
2020-06-03 14:19:32 +0400

    Exercise 1.33: Reading from the command line

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,15 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
 def portfolio_cost(filename):
     s = 0
-    with open(filename, 'r') as f:
-        for line in f:
-            row = line.split(',')
-            try:
-                s += int(row[1]) * float(row[2])
-            except ValueError:
-                print(f"Couldn't calculate line: {line}. Wrong or missing data.")
+    f = open(filename)
+    rows = csv.reader(f)
+    for row in rows:
+        try:
+            s += int(row[1]) * float(row[2])
+        except ValueError:
+            print(f"Couldn't calculate line: {row}. Wrong or missing data.")
+    f.close()
     return s
 
 cost = portfolio_cost('Data/portfolio.csv')
