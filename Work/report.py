4236e473f3e546669f05d4308113668d2fc3dc6e
Narek Baghdasaryan <narek2011n@gmail.com>
2020-06-03 18:45:07 +0400

    Updated to Exercise 2.7: Finding out if you can retire

--- Work/report.py
+++ Work/report.py
@@ -7,25 +7,44 @@ import csv
 
 def read_portfolio(filename):
     portfolio = []
-    f = open(filename)
-    headers = next(f)
-    rows = csv.reader(f)
-    for row in rows:
-        d = {
-            'name': row[0],
-            'shares': int(row[1]),
-            'price': float(row[2])
-        }
-        portfolio.append(d)
-    f.close()
+    with open(filename, 'r') as f:
+        rows = csv.reader(f)
+        headers = next(f)
+        for row in rows:
+            d = {
+                'name': row[0],
+                'shares': int(row[1]),
+                'price': float(row[2])
+            }
+            portfolio.append(d)
     return portfolio
 
 
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-else:
-    filename = 'Data/portfolio.csv'
+def read_prices(filename):
+    prices = {}
+    with open(filename, 'r') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+    return prices
 
-portfolio = read_portfolio(filename)
-print(portfolio)
 
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+
+portfolio_value = 0
+current_value = 0
+
+for stock in portfolio:
+    portfolio_value += stock['shares'] * stock['price']
+    current_value += stock['shares'] * prices[stock['name']]
+
+total_value = current_value - portfolio_value
+
+print(f'Old value: {portfolio_value}')
+print(f'Current value: {current_value}')
+
+print(f'Gain: {total_value:0.2f}' if total_value > 0 else f'Loss: {-total_value:0.2f}')
