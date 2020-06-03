d7b4bc83161e0168a60964ded7334479cad7c6ce
Narek Baghdasaryan <narek2011n@gmail.com>
2020-06-03 13:08:34 +0400

    Exercise 1.27: Reading a data file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,10 @@
 # pcost.py
 #
 # Exercise 1.27
+f = open('Data/portfolio.csv', 'r')
+next(f)
+s = 0
+for line in f:
+    row = line.strip().split(',')
+    s += int(row[1]) * float(row[2])
+print(f'Total cost: {s}')
