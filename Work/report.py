5eed2dc6c9c32d9253ba8449e5629df5fa61e084
Narek Baghdasaryan <narek2011n@gmail.com>
2020-06-03 17:50:03 +0400

    Exercise 2.5: List of Dictionaries

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,31 @@
 # report.py
 #
 # Exercise 2.4
+import sys
+import csv
+
+
+def read_portfolio(filename):
+    portfolio = []
+    f = open(filename)
+    headers = next(f)
+    rows = csv.reader(f)
+    for row in rows:
+        d = {
+            'name': row[0],
+            'shares': int(row[1]),
+            'price': float(row[2])
+        }
+        portfolio.append(d)
+    f.close()
+    return portfolio
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+portfolio = read_portfolio(filename)
+print(portfolio)
+
