ef2c15c8f48cf88129622ad077f2528b749b42fe
Narek Baghdasaryan <narek2011n@gmail.com>
2020-08-14 00:44:53 +0400

    Updated to A practical enumerate() example

--- Work/pcost.py
+++ Work/pcost.py
@@ -9,11 +9,13 @@ def portfolio_cost(filename):
     s = 0
     f = open(filename)
     rows = csv.reader(f)
-    for row in rows:
+    headers = next(rows)
+    for row_num, row in enumerate(rows, start=1):
         try:
             s += int(row[1]) * float(row[2])
         except ValueError:
-            print(f"Couldn't calculate line: {row}. Wrong or missing data.")
+            # print(f"Couldn't calculate line: {row}. Wrong or missing data.")
+            print(f"Row {row_num}: Couldn't convert: {row}")
     f.close()
     return s
 
