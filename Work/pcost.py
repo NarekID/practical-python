e638f0809a23d2d68338e70c9d8d8d4c388e4678
Narek Baghdasaryan <narek2011n@gmail.com>
2020-08-29 17:15:47 +0400

    Updated to Exercise 2.16: Using the zip() function

--- Work/pcost.py
+++ Work/pcost.py
@@ -6,18 +6,22 @@ import csv
 
 
 def portfolio_cost(filename):
-    s = 0
+    total = 0
     f = open(filename)
     rows = csv.reader(f)
     headers = next(rows)
     for row_num, row in enumerate(rows, start=1):
+        record = dict(zip(headers, row))
         try:
-            s += int(row[1]) * float(row[2])
+            shares_num = int(record['shares'])
+            price = float(record['price'])
+            total += shares_num * price
         except ValueError:
             # print(f"Couldn't calculate line: {row}. Wrong or missing data.")
-            print(f"Row {row_num}: Couldn't convert: {row}")
+            # print(f"Row {row_num}: Couldn't convert: {row}")
+            print(f'Row {row_num}: Bad row: {row}')
     f.close()
-    return s
+    return total
 
 
 if len(sys.argv) == 2:
